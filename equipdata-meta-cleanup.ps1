e6c1bb2f1df2586e7854ec59a6e3bfe5ffcc4d93
p3hndrx <richardphung@gmail.com>
2020-02-21 12:55:08 -0500

    ItemData Upload

---
+++ equipdata-meta-cleanup.ps1
@@ -0,0 +1,13 @@
+﻿$base = "D:\Projects\MLBB-EquipmentPlanner"
+$herolist = Get-Content -Raw -Path "$base\hero-list-test.json" | ConvertFrom-Json
+#$herolist = Get-Content -Raw -Path "$base\hero-list.json" | ConvertFrom-Json
+#$herolist.data
+
+$uri = "https://mapi.mobilelegends.com/hero/detail?id="
+
+$outfile = "$base\out.json"
+$today = Get-Date -Format "MM-dd-yyyy"
+
+$equipmentlist = Get-Content -Raw $outfile | ConvertFrom-Json
+$equiplist = $equipmentlist.data | Sort-Object -Unique equipment_id
+$equiplist | ConvertTo-Json
---
+++ equipdata-meta-scraper.ps1
@@ -0,0 +1,64 @@
+﻿$base = "D:\Projects\MLBB-EquipmentPlanner"
+#$herolist = Get-Content -Raw -Path "$base\hero-list-test.json" | ConvertFrom-Json
+$herolist = Get-Content -Raw -Path "$base\hero-list.json" | ConvertFrom-Json
+$herolist.data
+
+$uri = "https://mapi.mobilelegends.com/hero/detail?id="
+
+$outfile = "$base\out.json"
+$today = Get-Date -Format "MM-dd-yyyy"
+
+function head {
+
+    Write-Host 
+    "{
+        `"orig-source`":`"$uri`",
+        `"pull-date`":`"$today`",
+        `"data`":["| Out-File -Append $outfile
+}
+
+function scrape {
+
+    foreach($hero in $herolist.data)
+        {
+        #construct path
+        $path = $uri+$hero.heroid
+
+        #pull data
+        $response = Invoke-RestMethod -uri $path
+        $raw = $response.data.gear.out_pack | Select-Object | ConvertTo-Json #| Out-File -Append $outfile
+        
+
+         #format delimiter
+        if ($hero -eq $herolist.data[-1]){
+            $raw = $raw.replace("]","")
+            $raw = $raw.replace("[","")
+            Write-Host 
+            $raw+"]}" | Out-File -Append $outfile
+            
+            
+            } else {
+             Write-Host 
+            $raw = $raw.replace("]","")
+            $raw = $raw.replace("[","")               
+            $raw+"," | Out-File -Append $outfile
+            }
+       
+        
+        }
+}
+
+
+
+if(test-path $outfile){
+    Remove-Item $outfile
+    head
+    scrape
+    } else {
+    head
+    scrape
+    }
+
+$equipmentlist = Get-Content -Raw $outfile | ConvertFrom-Json
+$equiplist = $equipmentlist.data | Sort-Object -Unique equipment_id
+$equiplist | ConvertTo-Json | Out-File $outfile
