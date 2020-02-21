3178684793df71a841e371f4709b477639fe2d64
p3hndrx <richardphung@gmail.com>
2020-02-21 13:21:48 -0500

    Organizing SubFolders

---
+++ from MAPI/herodata-resource-scraper.ps1
@@ -0,0 +1,8 @@
+﻿$base = "D:\Projects\MLBB-EquipmentPlanner"
+$herolist = Get-Content -Raw -Path "$base\hero-list-test.json" | ConvertFrom-Json
+$herolist.data
+
+foreach($hero in $herolist.data)
+{
+$hero.heroid
+}
