7233852f19ee68284b98fc9d6ec648f8f529e561
p3hndrx <richardphung@gmail.com>
2020-02-26 16:48:36 -0500

    Updated Sample Item Meta

---
+++ helper/stitch-items.ps1
@@ -0,0 +1,9 @@
+﻿$head = gc "D:\Projects\MLBB-EquipmentPlanner\helper\head.txt"
+$list = gc "D:\Projects\MLBB-EquipmentPlanner\helper\list.txt"
+$tail = gc "D:\Projects\MLBB-EquipmentPlanner\helper\tail.txt"
+$out = "D:\Projects\MLBB-EquipmentPlanner\helper\out.json"
+
+foreach ($item in $list)
+{
+Write-Output "{`"item_name`":`"$item`","$tail"," | Out-File -append $out
+}
